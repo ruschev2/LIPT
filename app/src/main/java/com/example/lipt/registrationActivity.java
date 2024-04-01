da3530165b53c91a81b94dea1fe79676fc03fc87
Luis H <97080941+Ruschev2@users.noreply.github.com>
2024-04-01 16:43:56 -0700

    Added factories for login and registration views.

--- app/src/main/java/com/example/lipt/registrationActivity.java
+++ app/src/main/java/com/example/lipt/registrationActivity.java
@@ -2,13 +2,42 @@ package com.example.lipt;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.example.lipt.databinding.ActivityMainBinding;
+import com.example.lipt.databinding.ActivityRegistrationBinding;
 
 public class registrationActivity extends AppCompatActivity {
+
+    private ActivityRegistrationBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_registration);
+        binding = ActivityRegistrationBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+
+        //instantiating an interface of onclick listener
+        binding.registrationButton2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = MainActivity.mainToRegistrationFactory(getApplicationContext());
+                startActivity(intent);
+            }
+        });
+
     }
+
+    //intent factory
+    public static Intent registrationToMainFactory(Context context) {
+        return new Intent(context, registrationActivity.class);
+    }
+
+
+
+
 }
