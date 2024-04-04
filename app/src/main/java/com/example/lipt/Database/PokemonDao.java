e78138adc42451e2427bcba345ebafb7fbfe9917
Luis H <97080941+Ruschev2@users.noreply.github.com>
2024-04-03 20:30:22 -0700

    Created PokemonDao.java interface. This describes the pokemon data access object.

---
+++ app/src/main/java/com/example/lipt/Database/PokemonDao.java
@@ -0,0 +1,31 @@
+/**
+ * Luis Hernandez, Guillermo Zendejas
+ * April 3, 2024
+ * PokemonDao.java, this class describes the Data Access Object for the Pokemon entity
+ */
+
+package com.example.lipt.Database;
+import androidx.lifecycle.LiveData;
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.OnConflictStrategy;
+import androidx.room.Query;
+
+import java.util.List;
+
+@Dao
+public interface PokemonDao {
+
+    //for creating a new pokemon
+    @Insert(onConflict = OnConflictStrategy.ABORT)
+    void insert(Pokemon pokemon);
+
+    //for deleting an existing pokemon
+    @Delete()
+    void deletePokemon(Pokemon pokemon);
+
+    //for retrieving the entire list of Pokemon
+    @Query("SELECT * FROM pokemon_table ORDER BY pokedexNumber ASC")
+    LiveData<List<Pokemon>> getPokemonlist();
+}
