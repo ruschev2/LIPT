18378e4e000c06b0aaf516b0386f67f8f50949be
GmoZen <zendejas.guillermo@gmail.com>
2024-04-15 06:31:54 -0700

    Created initial fragments for admin users and all users, and connected them to BottomNavigation menu items within AdminActivity.

--- app/src/main/java/com/example/lipt/AdminActivity.java
+++ app/src/main/java/com/example/lipt/AdminActivity.java
@@ -9,6 +9,10 @@ import android.view.View;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
 import com.example.lipt.databinding.ActivityAdminBinding;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationBarView;
@@ -32,17 +36,26 @@ public class AdminActivity extends AppCompatActivity {
         bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
-                int itemId = menuItem.getItemId();
+                int selectedItemId = menuItem.getItemId();
+                Fragment adminUsersListFragment = new AdminUsersListFragment();
+                Fragment allUsersListFragment = new AllUsersListFragment();
+                Fragment nextFragment = adminUsersListFragment;
 
-                if (itemId == R.id.back_arrow_item) {
+
+                if (selectedItemId == R.id.back_arrow_item) {
                     Intent intent = MenuActivity.mainMenuFactory(getApplicationContext(), loggedInId);
                     startActivity(intent);
+                } else if (selectedItemId == R.id.admin_users_item) {
+                    nextFragment = adminUsersListFragment;
+                } else if (selectedItemId == R.id.all_users) {
+                    nextFragment = allUsersListFragment;
                 }
-
-
+                switchFragment(nextFragment);
                 return true;
             }
         });
+
+        bottomNavigationView.setSelectedItemId(R.id.all_users);
 
     }
 
@@ -57,4 +70,13 @@ public class AdminActivity extends AppCompatActivity {
         intent.putExtra(ADMIN_ACTIVITY_USER_ID, adminUserId);
         return intent;
     }
+
+    //todo uncomment after setting up fragments
+    private void switchFragment(Fragment newFragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.admin_frame_layout, newFragment);
+        fragmentTransaction.commit();
+    }
+
 }
